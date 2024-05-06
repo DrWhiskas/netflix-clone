44b3cbe9887f5a6394ed575a95a8d5a47f4e17a4
unknown <kayv@hotmail.fr>
2024-05-06 16:16:46 +0200

    header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,36 +1,60 @@
 import React from "react";
 import './header.css'
+import { Search, Bell, ChevronDown } from "lucide-react";
 export default function Header(){
     return (
 			<header className="header">
-					<div className="header__logo">NETFLIX</div>
-					<ul className="header__nav">
-						<li className="header__nav__links">
-							<a className="header__nav__links__link" href="#">
-								Home
-							</a>
-						</li>
-						<li className="header__nav__links">
-							<a className="header__nav__links__link" href="#">
-								Series
-							</a>
-						</li>
-						<li className="header__nav__links">
-							<a className="header__nav__links__link" href="#">
-								Films
-							</a>
-						</li>
-						<li className="header__nav__links">
-							<a className="header__nav__links__link" href="#">
-								New & Popular
-							</a>
-						</li>
-						<li className="header__nav__links">
-							<a className="header__nav__links__link" href="#">
-								My List
-							</a>
-						</li>
-					</ul>
+				<div className="nav">
+				<div className="header__logo">NETFLIX</div>
+				<ul className="header__nav">
+					<li className="header__nav__links">
+						<a className="header__nav__links__link" href="#">
+							Home
+						</a>
+					</li>
+					<li className="header__nav__links">
+						<a className="header__nav__links__link" href="#">
+							Series
+						</a>
+					</li>
+					<li className="header__nav__links">
+						<a className="header__nav__links__link" href="#">
+							Films
+						</a>
+					</li>
+					<li className="header__nav__links">
+						<a className="header__nav__links__link" href="#">
+							New&Popular
+						</a>
+					</li>
+					<li className="header__nav__links">
+						<a className="header__nav__links__link" href="#">
+							List
+						</a>
+					</li>
+				</ul>
+				</div>
+				<ul className="header__nav profil">
+					<li className="header__nav__links">
+						<Search className="white" />
+					</li>
+					<li className="header__nav__links">
+						<a className="header__nav__links__link" href="#">
+							Kids
+						</a>
+					</li>
+					<li className="header__nav__links">
+						<Bell className="white" />
+					</li>
+					<li className="header__nav__links">
+						<img
+							src="https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2022/06/12_Jinx.jpg"
+							alt="icon"
+							className="header__list__container__icon"
+						/>
+						<ChevronDown className="white arrow" />
+					</li>
+				</ul>
 			</header>
 		);
 }
